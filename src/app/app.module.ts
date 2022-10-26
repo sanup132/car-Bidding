00b086635ff62949db994c9c670452cfcb16192a
Sanup <sanup.cse@gmail.com>
2022-10-26 19:45:47 +0530

    uploaded project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { OwnerDetailsComponent } from './components/owner-details/owner-details.component';
+import { CarInBiddingComponent } from './components/car-in-bidding/car-in-bidding.component';
+import { CarSoldComponent } from './components/car-sold/car-sold.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddCarComponent,
+    OwnerDetailsComponent,
+    CarInBiddingComponent,
+    CarSoldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/car-sold/car-sold.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-sold',
+  templateUrl: './car-sold.component.html',
+  styleUrls: ['./car-sold.component.css']
+})
+export class CarSoldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
